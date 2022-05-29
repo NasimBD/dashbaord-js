c2a550885c5585e3dcf1813540f66507941da58d
nasimBD <nasim.bdilmaghani@gmail.com>
2022-05-29 22:19:55 +0430

    init

---
+++ orders.js
@@ -0,0 +1,62 @@
+const Orders = [
+    {
+        productName: 'AHGJ',
+        productNumber: '4532',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GFYJ',
+        productNumber: '65',
+        paymentStatus: 'Refused',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'ZRT',
+        productNumber: '87',
+        paymentStatus: 'Pending',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'JHKUB TYT',
+        productNumber: '89080',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'MNJH>',
+        productNumber: '2132',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DRF',
+        productNumber: '66786',
+        paymentStatus: 'Refused',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'PIJ',
+        productNumber: '45-54',
+        paymentStatus: 'Pending',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'RYTH NB',
+        productNumber: '1243',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'UYHJ',
+        productNumber: '111',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'VYFG',
+        productNumber: '009',
+        paymentStatus: 'Refused',
+        shipping: 'Declined'
+    }
+];
